c69f400de78b92113c36aa0ea5741e1ffc446c09
khanh02092006 <dinhkhanh1881@gmail.com>
2024-11-17 17:13:23 +0700

    sesiion 2 _ ex05

---
+++ sesiion 2 _ ex05.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+int main (){
+	int dai = 4;
+	int rong = 5;
+	int chuvi = 2 * ( dai + rong );
+	printf ("chu vi là : %d\n",chuvi) ;
+	
+	int dientich = dai * rong;
+	printf (" dien tich là :%d\n",dientich);
+	
+	return 0;
+	}
+
