f4657d6ec0cad86c256374a8c60743b4f97ff631
khanh02092006 <dinhkhanh1881@gmail.com>
2024-11-17 16:45:01 +0700

    session 2 _ex03.

---
+++ session 2 _ex03.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int a = 2;
+	int b = 3;
+	int hieu = a + b;
+	printf("hieu là %d\n",hieu);
+	
+	int tich = a * b;
+	printf("tich là %d\n",tich);
+	
+	
+	int thuong = a / b;
+	printf("thuong là %d\n",thuong);
+	
+	return 0;
+	}
