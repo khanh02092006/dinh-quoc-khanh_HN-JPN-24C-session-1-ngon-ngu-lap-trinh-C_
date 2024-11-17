74ffa3335ad839e2f7a0be4245afd4af92425a86
khanh02092006 <dinhkhanh1881@gmail.com>
2024-11-17 17:01:04 +0700

    sesion 2 _ ex04.

---
+++ sesion 2 _ ex04.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int canh = 4;
+	int chuvi = 4* canh ;
+	printf ("chu vi %d\n",chuvi);
+	
+	int dientich = canh * canh;
+	printf ("dien tich %d\n",dientich);
+	return 0;
+	}
+	
+
+
